2203821e1b73f834681c30ddaadf045d0cbbb378
alirezaarmn <alirezaaramoun@gmail.com>
2024-01-27 11:59:11 -0500

    minor changes

--- mywiki/main.py
+++ mywiki/main.py
@@ -1,23 +1,46 @@
+# 1. expose the local machine to the internet: ssh -R 80:127.0.0.1:5000 serveo.net
+# 2. set webhook with the output of previous step
 #--- set webhook: 
 #       https://api.telegram.org/bot6546517474:AAHBVAesenlhwFL_altk4E-dqwO6xtqh40k/setWebhook?url=https://0e67e13920a0256c617b52a8b15a6eb8.serveo.net
 
+# TODO: make the scap_wiki a class 
+# TODO: get an instance class of scrap_wiki
+# TODO: provide button for the categories
 
 from flask import Flask
 from flask import request
 from flask import Response
+import requests
 import json
 
 app = Flask(__name__)
+
+TOKEN = '6546517474:AAHBVAesenlhwFL_altk4E-dqwO6xtqh40k'
+txt_welcome = "خوش آمدید. \n لطفا یکی از مغازه ها رو انتخاب کنید"
+btn_saayi = '{"text": "پارک ساعی", "callback_data": "saayi%s" }'
+btn_hovize = '{"text": "هویزه", "callback_data": "hovize%s" }'
 
 def write_json(data, filename='response.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
+def sendWelcome(chatId):
+    url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage"
+    data = {'chat_id': chatId, 'text': txt_welcome, 'parse_mode': 'HTML',
+    'reply_markup': '{"inline_keyboard": [[%s,%s]]}' % (btn_saayi, btn_hovize)}
+    r = requests.post(url, data=data)
+    print(r.text)
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        msg = request.get_json()
-        write_json(msg, 'telgram_logs.json')
+        raw_json = request.get_json()
+        print(raw_json)
+        if 'message' in raw_json:
+            chat_id = str(raw_json["message"]["chat"]["id"])
+            if raw_json["message"]["text"] == '/start':
+                sendWelcome(chat_id)
+        # write_json(msg, 'telgram_logs.json')
         return Response('ok', status=200)
     else:
         return '<h1> My Wiki Bot</h1>'
