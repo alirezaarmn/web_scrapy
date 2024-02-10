af7bea7f8c977174dfb2602af23fe63f0e07ecc8
alirezaarmn <alirezaaramoun@gmail.com>
2024-02-09 22:13:31 -0500

    Retriving categories from the wiki and showing them in menu

--- mywiki/main.py
+++ mywiki/main.py
@@ -3,7 +3,7 @@
 #--- set webhook: 
 #       https://api.telegram.org/bot6546517474:AAHBVAesenlhwFL_altk4E-dqwO6xtqh40k/setWebhook?url=https://0e67e13920a0256c617b52a8b15a6eb8.serveo.net
 
-# TODO: provide button for the categories
+# TODO: scraping should be perform once and at the startup, not in the /start command
 
 from flask import Flask
 from flask import request
@@ -16,23 +16,30 @@ from scrap_wiki import scrap
 app = Flask(__name__)
 
 TOKEN = '6546517474:AAHBVAesenlhwFL_altk4E-dqwO6xtqh40k'
-txt_welcome = "خوش آمدید. \n لطفا یکی از مغازه ها رو انتخاب کنید"
-btn_saayi = '{"text": "پارک ساعی", "callback_data": "saayi%s" }'
-btn_hovize = '{"text": "هویزه", "callback_data": "hovize%s" }'
+txt_welcome = "Welcome to my personal wiki. "
 
 def write_json(data, filename='response.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
-def sendWelcome(chatId):
+def sendWelcome(chatId, first_name):
     url = "https://api.telegram.org/bot" + TOKEN + "/sendMessage"
-    data = {'chat_id': chatId, 'text': txt_welcome, 'parse_mode': 'HTML',
-    'reply_markup': '{"inline_keyboard": [[%s,%s]]}' % (btn_saayi, btn_hovize)}
+    data = {'chat_id': chatId, "text": "%s!\n%s"%(first_name,txt_welcome)}
+    r = requests.post(url, data=data)
 
+def sendMenu():
+    sample = scrap()
+    temp = sample.getCategories()
+    print(sample.getCategories())
 
+    cat = ''
+    for item in temp:
+        cat += '{"command":"%s", "description":"%s"},'%(item[0].lower(),item)
+    cat = cat[:-1] #TODO: is there any better solution for removing the , at the end of string?
+    print(cat)
     url = "https://api.telegram.org/bot" + TOKEN + "/setMyCommands"
-    data = {'commands': [{'command': "prueba", 'description': "Esto es una prueba"}],
-    'scope': {type: "all_group_chats"}}
+    # data = {'commands': '[{"command":"h", "description":"Home"},{"command":"a", "description":"aaa"}]'}
+    data = {'commands': '[%s]'%cat}
 
     r = requests.post(url, data=data)
     print(r.text)
@@ -44,13 +51,13 @@ def index():
         print(raw_json)
         if 'message' in raw_json:
             chat_id = str(raw_json["message"]["chat"]["id"])
+            first_name = str(raw_json["message"]["chat"]["first_name"])
             if raw_json["message"]["text"] == '/start':
-                sendWelcome(chat_id)
+                sendMenu()
+                sendWelcome(chat_id, first_name)
         # write_json(msg, 'telgram_logs.json')
         return Response('ok', status=200)
     else:
-        sample = scrap()
-        print(sample.getCategories())
         return '<h1> My Wiki Bot</h1>'
 
 if __name__ == '__main__':
